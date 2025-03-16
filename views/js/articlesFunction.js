86010d7286587aa32215c6e2242ab88963b153b5
BentsuKun <cherd8524@gmail.com>
2025-03-16 22:00:53 +0700

    อัพเดต : หน้าบทความ
    เกี่ยวกับการแสดงไฟล์บทความ และสไตล์ของหน้า

--- views/js/articlesFunction.js
+++ views/js/articlesFunction.js
@@ -19,27 +19,38 @@ socket.on('Articles-data', (articles) => {
             articleLink += '/preview';
         }
         
-        articlesDisplay.innerHTML += `
+        // articlesDisplay.innerHTML += `
         
-            <div class="accordion mb-2" id="accordion_${countArticle}">
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#${articleId}" aria-expanded="false" aria-controls="${articleId}">
-                            <span class="fw-bold me-2">${countArticle}.</span> ${articleTopic}
-                        </button>
-                    </h2>
-                    <div id="${articleId}" class="accordion-collapse collapse" data-bs-parent="#accordion_${countArticle}">
-                        <div class="row px-5 py-3 d-flex justify-content-between">
-                            <div class="articleUploader col">โพสต์โดย : ${articleAuthor}</div>
-                            <div class="articleDTUpload col">วันที่โพสต์ ${articleDateTime.getDate()}/${articleDateTime.getMonth()}/${articleDateTime.getFullYear()+543} เวลา ${articleDateTime.getHours()}.${articleDateTime.getMinutes()} น.</div>
-                        </div>
-                        <div class="accordion-body d-flex justify-content-center pt-0">
-                            <iframe src="${articleLink}" width="100%" height="1180" allow="autoplay"></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        //     <div class="accordion mb-2" id="accordion_${countArticle}">
+        //         <div class="accordion-item">
+        //             <h2 class="accordion-header">
+        //                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+        //                     data-bs-target="#${articleId}" aria-expanded="false" aria-controls="${articleId}">
+        //                     <span class="fw-bold me-2">${countArticle}.</span> ${articleTopic}
+        //                 </button>
+        //             </h2>
+        //             <div id="${articleId}" class="accordion-collapse collapse" data-bs-parent="#accordion_${countArticle}">
+        //                 <div class="row px-5 py-3 d-flex justify-content-between">
+        //                     <div class="articleUploader col">โพสต์โดย : ${articleAuthor}</div>
+        //                     <div class="articleDTUpload col">วันที่โพสต์ ${articleDateTime.getDate()}/${articleDateTime.getMonth()}/${articleDateTime.getFullYear()+543} เวลา ${articleDateTime.getHours()}.${articleDateTime.getMinutes()} น.</div>
+        //                 </div>
+        //                 <div class="accordion-body d-flex justify-content-center pt-0">
+        //                     <iframe src="${articleLink}" width="100%" height="1180" allow="autoplay"></iframe>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        
+        // `;
+
+        articlesDisplay.innerHTML += `
+
+            <a href="${article.linkSharing}" target="_blank" class="btn btn-outline-light mb-2 py-3 w-100">
+                <span class="d-flex justify-content-between px-2">
+                    <div>${countArticle}. ${articleTopic}</div>
+                    <div><i class="ri-external-link-line"></i></div>
+                </span>
+            </a>
         
         `;
 
