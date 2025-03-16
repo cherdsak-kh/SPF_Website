c2888101a8316daba8e06c3904a666bdadf74b9a
BentsuKun <cherd8524@gmail.com>
2025-03-16 14:12:00 +0700

    เปลี่ยนชื่อ db

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ async function mongodbExecute() {
     }
 
     // * ดึงข้อมูลทั้งหมดจากคอลเลกชัน Events ที่ eventName มีคำว่า "MilSim" ด้วยการใส่ { eventName: /MilSim/ }
-    // const allData = client.db('spf_system_db').collection('events');
+    // const allData = client.db('spf_system').collection('events');
     // const data = await allData.find({}).toArray();
 
     // Sort by dateTime in descending order
@@ -84,7 +84,7 @@ app.get('/', async (req, res) => {
 
     try {
         await client.connect()
-        const data = client.db('spf_system_db').collection('menu')
+        const data = client.db('spf_system').collection('menu')
         const Menu = await data.find().toArray()
 
         res.render('pages/index', {
@@ -112,7 +112,7 @@ app.get('/about', async (req, res) => {
 
     try {
         await client.connect()
-        const data = client.db('spf_system_db').collection('menu')
+        const data = client.db('spf_system').collection('menu')
         const Menu = await data.find().toArray()
 
         res.render('pages/about', {
@@ -140,7 +140,7 @@ app.get('/termsandconditions', async (req, res) => {
 
     try {
         await client.connect()
-        const data = client.db('spf_system_db').collection('menu')
+        const data = client.db('spf_system').collection('menu')
         const Menu = await data.find().toArray()
 
         res.render('pages/rules', {
@@ -168,7 +168,7 @@ app.get('/event', async (req, res) => {
 
     try {
         await client.connect()
-        const data1 = client.db('spf_system_db').collection('menu')
+        const data1 = client.db('spf_system').collection('menu')
         const Menu = await data1.find().toArray()
 
         res.render('pages/event', {
@@ -196,7 +196,7 @@ app.get('/articles', async (req, res) => {
 
     try {
         await client.connect()
-        const data1 = client.db('spf_system_db').collection('menu')
+        const data1 = client.db('spf_system').collection('menu')
         const Menu = await data1.find().toArray()
 
         res.render('pages/articles', {
@@ -224,7 +224,7 @@ app.get('/cookie-policy', async (req, res) => {
 
     try {
         await client.connect()
-        const data = client.db('spf_system_db').collection('menu')
+        const data = client.db('spf_system').collection('menu')
         const Menu = await data.find().toArray()
 
         res.render('pages/cookie-policy', {
@@ -284,7 +284,7 @@ io.on('connection', (socket) => {
             await client.connect();
             // console.log('== Send events data ==> Start');
 
-            const allData = client.db('spf_system_db').collection('events');
+            const allData = client.db('spf_system').collection('events');
             const data = await allData.find({}).toArray();
 
             socket.emit('Events-data', data);  // Send the retrieved data to the client
@@ -303,7 +303,7 @@ io.on('connection', (socket) => {
         try {
             await client.connect();
 
-            const allData = client.db('spf_system_db').collection('articles');
+            const allData = client.db('spf_system').collection('articles');
             const data = await allData.find({}).toArray();
 
             socket.emit('Articles-data', data);
