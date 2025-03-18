1f69aefd7f1d4f1c4a89f03936430122970b842d
BentsuKun <cherd8524@gmail.com>
2025-03-18 14:46:53 +0700

    Update eventFunction.js

--- views/js/eventFunction.js
+++ views/js/eventFunction.js
@@ -503,6 +503,7 @@ socket.on('Events-data', (events) => {
         let hasEvent = '';
 
         const thaiWeekdays = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์'];
+        const thaiMonths = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
 
         for (const event of icmEvents) {
             const eventDateTime = new Date(event.dateTime);
