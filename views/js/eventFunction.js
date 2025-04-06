73ff69d430b6d4f82afc569b7312c0fd56df91b5
BentsuKun <cherd8524@gmail.com>
2025-04-06 14:18:54 +0700

    Update eventFunction.js

--- views/js/eventFunction.js
+++ views/js/eventFunction.js
@@ -99,6 +99,7 @@ socket.on('Events-data', (getEvents) => {
 
             for (const event of events) {
                 const eventDate = new Date(event.eventDateTime);
+                console.log(eventDate);
                 if (eventDate.getDate() === day && eventDate.getMonth() === month && eventDate.getFullYear() === year) {
                     if (event.eventType === 'Mission Day') {
                         col.classList.add('position-relative');
